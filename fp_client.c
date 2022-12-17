c9d3ea8637a1bc580574722a294160d61847a7c3
danieltorres4 <dst990502@outlook.com>
2022-12-17 17:45:12 -0600

    Adding headers

--- fp_client.c
+++ fp_client.c
@@ -6,3 +6,14 @@
 /********  HERRERA GODINA ADRIANA JOCELYN  ********/
 /********   SANCHEZ TORRES SERGIO DANIEL   ********/
 /**************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#define MAXDATASIZE 100
+#define MAXDATASIZE_RESP 20000
--- fp_server.c
+++ fp_server.c
@@ -6,3 +6,14 @@
 /********  HERRERA GODINA ADRIANA JOCELYN  ********/
 /********   SANCHEZ TORRES SERGIO DANIEL   ********/
 /**************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
