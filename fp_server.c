c442cc2c0a93f9ca47b550cff96cf14f77080217
danieltorres4 <dst990502@outlook.com>
2022-12-23 12:27:22 -0600

    Adding new functions

--- fp_server.c
+++ fp_server.c
@@ -24,6 +24,21 @@
 
 #define LENGTH 20000
 
+int spawn(char* program, char** arg_list) {
+    pid_t child_pid;
+
+    child_pid = fork();
+
+    if(child_pid != 0) {
+        return child_pid;
+    } else {
+        execvp(program, arg_list);
+
+        fprintf(stderr, "An error has been occurred at execvp()\n");
+        abort();
+    }
+}
+
 int main(int argc, char *argv[]){
     int numbytes;
     char buf[150];
@@ -81,11 +96,37 @@ int main(int argc, char *argv[]){
 
         printf("Server: Client is connected from: %s\n", inet_ntoa(client.sin_addr));
 
-        if((numbytes = recv(client_fd, buf, 100-1, 0)) == -1) {
-            perror("recv");
-            exit(1);
-        }
+        if(fork() == 0) {
+            close(server_fd);
+
+            do {
+                if((numbytes = recv(client_fd, buf, 100-1, 0)) == -1) {
+                perror("recv");
+                exit(1);
+                }
+
+                printf("Client's command: %s\n", buf);
+
+                buf[numbytes] = '\0';
+
+                fgets(buf, LENGTH, stdin);
+
+                
+
+                send(client_fd, buf, strlen(buf), 0);
+            } while(strcmp(buf, "adios\n") != 0);
+
+            printf("Client's connection has been finished");
+            close(client_fd);
+            exit(0);
+        } else {
+            close(client_fd);
+        } 
+
     }
-    
+
+    close(server_fd);
+    shutdown(server_fd, SHUT_RDWR);
+    exit(0);
     
 }
